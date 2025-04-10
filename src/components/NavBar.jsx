f4d6632cb81671a1415aefdef406815dc249ff6f
tombrt265 <tom@familie-bielert.de>
2025-04-10 23:26:57 +0200

    add first look of navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,52 @@
+import splitMatesLogo from "../assets/coin.png";
+
 function NavBar() {
   return (
-    <nav
-      class="navbar"
-      style="background-color: #e3f2fd;"
-      data-bs-theme="light"
-    ></nav>
+    <>
+      <nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img
+              src={splitMatesLogo}
+              style={{ height: "1.5em", padding: "0 5px" }}
+            />
+            SplitMates
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
