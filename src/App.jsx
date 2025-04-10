8a18bc2354e599dbb862b1c2af53f233dc6ce1ad
Emre Dirlik <emred@deutschebahn.com>
2025-04-10 14:53:24 +0200

    Add Header, include header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to React</h1>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <div>
+            <h1>  ich furze die HWR voll  </h1>
+        </div>
+    );
+}
+export default Header;
