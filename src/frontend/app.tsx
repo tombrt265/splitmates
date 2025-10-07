519bc7939d257ec8e2cf102286a0771490ee3f33
tombrt265 <tom@familie-bielert.de>
2025-10-07 13:01:20 +0200

    wip

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Route, Routes } from "react-router-dom";
 import { PageLoader } from "./components/page-loader";
-import { AuthenticationGuard } from "./components/authentification-guard";
 import { CallbackPage } from "./pages/callback-page";
 import { HomePage } from "./pages/home-page";
 import { NotFoundPage } from "./pages/not-found-page";
@@ -22,10 +21,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route
-        path="/profile"
-        element={<AuthenticationGuard component={GuardedPage} />}
-      />
+      <Route path="/profile" element={<GuardedPage />} />
       <Route path="/groups" element={<GroupsPage />} />
       <Route path="/callback" element={<CallbackPage />} />
       <Route path="*" element={<NotFoundPage />} />
