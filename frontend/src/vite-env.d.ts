8c5af5432ec594c1b73efdb30e6ee32d1d167635
tombrt265 <tom@familie-bielert.de>
2025-10-09 21:31:35 +0200

    fix

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_AUTH0_DOMAIN: string;
-  readonly VITE_AUTH0_CLIENT_ID: string;
-  readonly VITE_AUTH0_CALLBACK_URL: string;
-  // hier kannst du weitere VITE_ Variablen hinzufügen
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
