21e029cc95407e2684623116d3b8778c536acc39
Tom Bielert <tom.bielert@deutschebahn.com>
2026-01-16 00:20:53 +0100

    add group tests

--- backend/tests/groups.test.ts
+++ backend/tests/groups.test.ts
@@ -1,57 +1,18 @@
-import request from "supertest";
 import { assert, describe, it, beforeEach, afterAll } from "vitest";
-import { app } from "../src/app";
 import { pool, exec } from "../src/db";
+import {
+  createUser,
+  createGroup,
+  createInviteLink,
+  getUserGroups,
+  joinGroup,
+  deleteGroup,
+  createExpense,
+  getGroupOverview,
+} from "./utils";
+import { Expense } from "./models";
 
-let client: import("pg").PoolClient;
-
-// ---------------- Types ----------------
-interface User {
-  id: string;
-  username: string;
-  email: string;
-  auth0_sub: string;
-  picture?: string;
-}
-
-interface Group {
-  id: string;
-  name: string;
-  category: string;
-  avatar_url?: string;
-  auth0_sub: string;
-}
-
-// ---------------- Helpers ----------------
-async function createTestUser(id: number): Promise<User> {
-  const res = await request(app)
-    .post("/api/users/signup")
-    .send({
-      username: `test_username_${id}`,
-      email: `test_email_${id}@test.com`,
-      auth0_sub: `test_auth0_sub_${id}`,
-      picture: "test_picture",
-    });
-  return res.body as User;
-}
-
-async function createGroup(user: User): Promise<Group> {
-  const res = await request(app).post("/api/groups").send({
-    name: "test_groupname",
-    category: "test_category",
-    avatar_url: "test_avatar_url",
-    auth0_sub: user.auth0_sub,
-  });
-  return res.body as Group;
-}
-
-async function getUserGroups(user: User): Promise<Group[]> {
-  const res = await request(app).get(`/api/groups?user_id=${user.auth0_sub}`);
-  return res.body as Group[];
-}
-
-// ---------------- Tests ----------------
-describe("Groups are being created", () => {
+describe("create a group and ...", () => {
   beforeEach(async () => {
     await exec("DELETE FROM expense_debtors");
     await exec("DELETE FROM expenses");
@@ -68,9 +29,9 @@ describe("Groups are being created", () => {
     await exec("ALTER SEQUENCE invite_tokens_id_seq RESTART WITH 1");
   });
 
-  it("should create a group and list it under the user", async () => {
+  it("... list it under the user", async () => {
     // WHEN
-    const user_1 = await createTestUser(1);
+    const user_1 = await createUser(1);
     const group = await createGroup(user_1);
 
     // THEN
@@ -82,7 +43,116 @@ describe("Groups are being created", () => {
     assert.equal(user_groups[0].category, "test_category");
   });
 
+  it("... delete it", async () => {
+    // WHEN
+    const user_1 = await createUser(1);
+    const group = await createGroup(user_1);
+
+    // THEN
+    deleteGroup(group);
+    const user_groups = await getUserGroups(user_1);
+
+    // ASSERT
+    assert.equal(user_groups.length, 0);
+  });
+
+  it("... receive an invite link", async () => {
+    // WHEN
+    const user_1 = await createUser(1);
+    const user_2 = await createUser(2);
+    const group = await createGroup(user_1);
+
+    // THEN
+    const invite_link = await createInviteLink(group);
+    const token = new URL(invite_link).searchParams.get("token");
+    const group_id = await joinGroup(token!, user_2);
+    const user_2_groups = await getUserGroups(user_2);
+
+    // ASSERT
+    assert.equal(user_2_groups.length, 1);
+    assert.equal(group.id, group_id);
+  });
+
   afterAll(async () => {
+    await exec("DELETE FROM expense_debtors");
+    await exec("DELETE FROM expenses");
+    await exec("DELETE FROM group_members");
+    await exec("DELETE FROM invite_tokens");
+    await exec("DELETE FROM groups");
+    await exec("DELETE FROM users");
+
+    await exec("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE groups_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE group_members_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expenses_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expense_debtors_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE invite_tokens_id_seq RESTART WITH 1");
+  });
+});
+
+describe("add an expense and ...", () => {
+  beforeEach(async () => {
+    await exec("DELETE FROM expense_debtors");
+    await exec("DELETE FROM expenses");
+    await exec("DELETE FROM group_members");
+    await exec("DELETE FROM invite_tokens");
+    await exec("DELETE FROM groups");
+    await exec("DELETE FROM users");
+
+    await exec("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE groups_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE group_members_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expenses_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expense_debtors_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE invite_tokens_id_seq RESTART WITH 1");
+  });
+
+  it("... get an overview of the group", async () => {
+    // WHEN
+    const user_1 = await createUser(1);
+    const user_2 = await createUser(2);
+    const group = await createGroup(user_1);
+    const invite_link = await createInviteLink(group);
+    const token = new URL(invite_link).searchParams.get("token");
+    const group_id = await joinGroup(token!, user_2);
+
+    let expense: Expense = {
+      id: "1",
+      group_id: group.id,
+      payer_id: user_1.id,
+      amount_cents: "1000",
+      currency: "€",
+      category: "Food",
+      description: "Kebab",
+      created_at: "",
+      debtors: [user_2.id],
+    };
+
+    // THEN
+    expense = await createExpense(expense);
+    const overview = await getGroupOverview(group);
+
+    // ASSERT
+    assert.equal(overview.category, "test_category");
+    assert.equal(overview.members.length, 2);
+    assert.equal(overview.expenses.length, 1);
+    assert.equal(overview.expenses[0].debtors.length, 1);
+  });
+
+  afterAll(async () => {
+    await exec("DELETE FROM expense_debtors");
+    await exec("DELETE FROM expenses");
+    await exec("DELETE FROM group_members");
+    await exec("DELETE FROM invite_tokens");
+    await exec("DELETE FROM groups");
+    await exec("DELETE FROM users");
+
+    await exec("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE groups_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE group_members_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expenses_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE expense_debtors_id_seq RESTART WITH 1");
+    await exec("ALTER SEQUENCE invite_tokens_id_seq RESTART WITH 1");
     await pool.end();
   });
 });
---
+++ backend/tests/utils.ts
@@ -0,0 +1,66 @@
+import { User, Group, Expense, GroupOverview } from "./models";
+import { app } from "../src/app";
+import request from "supertest";
+
+export async function createUser(id: number): Promise<User> {
+  const res = await request(app)
+    .post("/api/users/signup")
+    .send({
+      username: `test_username_${id}`,
+      email: `test_email_${id}@test.com`,
+      auth0_sub: `test_auth0_sub_${id}`,
+      picture: "test_picture",
+    });
+  return res.body as User;
+}
+
+export async function createGroup(user: User): Promise<Group> {
+  const res = await request(app).post("/api/groups").send({
+    name: "test_groupname",
+    category: "test_category",
+    avatar_url: "test_avatar_url",
+    auth0_sub: user.auth0_sub,
+  });
+  return res.body as Group;
+}
+
+export async function deleteGroup(group: Group) {
+  const res = await request(app).delete(`/api/groups/${group.id}`);
+}
+
+export async function getUserGroups(user: User): Promise<Group[]> {
+  const res = await request(app).get(`/api/groups?user_id=${user.auth0_sub}`);
+  return res.body as Group[];
+}
+
+export async function createInviteLink(group: Group): Promise<string> {
+  const res = await request(app).post(`/api/groups/${group.id}/invite`);
+  return res.body.invite_link;
+}
+
+export async function joinGroup(token: string, user: User): Promise<string> {
+  const res = await request(app).post(`/api/groups/join`).send({
+    token: token,
+    auth0_sub: user.auth0_sub,
+  });
+  return res.body.group_id;
+}
+
+export async function createExpense(expense: Expense): Promise<Expense> {
+  const res = await request(app)
+    .post(`/api/groups/${expense.group_id}/expenses`)
+    .send({
+      payerId: expense.payer_id,
+      amount: Number(expense.amount_cents) * 100,
+      currency: expense.currency,
+      category: expense.category,
+      description: expense.description,
+      debtors: expense.debtors,
+    });
+  return res.body;
+}
+
+export async function getGroupOverview(group: Group): Promise<GroupOverview> {
+  const res = await request(app).get(`/api/groups/${group.id}/overview`);
+  return res.body;
+}
